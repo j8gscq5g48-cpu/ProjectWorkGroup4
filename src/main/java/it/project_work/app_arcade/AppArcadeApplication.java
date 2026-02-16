76739351ad78bb60b115b87efd5c32c78d77052a
Jekosolitario <ichchozuhir@gmail.com>
2026-02-16 15:44:24 +0100

    chore: bootstrap Spring Boot project

---
+++ src/main/java/it/project_work/app_arcade/AppArcadeApplication.java
@@ -0,0 +1,13 @@
+package it.project_work.app_arcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppArcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/project_work/app_arcade/AppArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.project_work.app_arcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

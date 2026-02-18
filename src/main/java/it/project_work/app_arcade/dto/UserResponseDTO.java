0fc55144ffd5aa0e906501b5d81624379cee8c2c
Gilda Raccanelli <tua.email@esempio.com>
2026-02-18 11:26:50 +0100

    aggiunti i campi

--- src/main/java/it/project_work/app_arcade/dto/UserResponseDTO.java
+++ src/main/java/it/project_work/app_arcade/dto/UserResponseDTO.java
@@ -7,4 +7,7 @@ public class UserResponseDTO {
     private Long id;
     private String username;
     private String email;
+    private String role;        
+    private Integer level;      
+    private String avatarUrl;   
 }

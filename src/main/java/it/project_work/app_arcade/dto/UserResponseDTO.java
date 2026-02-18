757bc6161295320182e41b6e9ef9fda280582e23
Gilda Raccanelli <tua.email@esempio.com>
2026-02-18 10:30:51 +0100

    Aggiunto aggiornamenti USerrResponceDTO

--- src/main/java/it/project_work/app_arcade/dto/UserResponseDTO.java
+++ src/main/java/it/project_work/app_arcade/dto/UserResponseDTO.java
@@ -1,5 +1,10 @@
 package it.project_work.app_arcade.dto;
 
+import lombok.Data;
+
+@Data
 public class UserResponseDTO {
-    
+    private Long id;
+    private String username;
+    private String email;
 }
